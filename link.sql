f9ea6d9cbb45d0014d7e3f6f42bbdcdb582b5eeb
ayoung8 <ayoung8@users.noreply.github.com>
2016-10-21 12:09:40 -0400

    Delete link.sql

--- link.sql
+++
@@ -1,4 +0,0 @@
-select All_Products.*, Manufacturers.*
-from All_Products
-join Manufacturers
-  on Manufacturers.ManuCode like concat(All_Products.MFRCode,'%') WHERE Manufacturers.MfgName LIKE '%'"ABBOTT"'%'
