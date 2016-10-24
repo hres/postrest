bfb378cab7b7efc0bb2bd63b00ca6f8cb0762fd7
ayoung8 <ayoung8@users.noreply.github.com>
2016-10-24 10:15:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ $config = parse_ini_file('./db.ini');
 $dbhandle = mysqli_connect($config['hostname'], $config['username'], $config['password'],$config['dbname']) or die("Unable to connect to Server");
 // Create dropdown for Categories
 	//<p> To view the list of companies and their products, please select a category and a sub-category. </p>
-echo "Category: <select name='searchin'>";
+echo "Category: <select name='searchcategories'>";
 	$querycategories_prepare = mysqli_stmt_init($dbhandle);
-	mysqli_stmt_prepare($querycategories_prepare, SELECT `HeaderE` FROM `Categories`);
-	mysqli_stmt_bind_param($querycategories_prepare, "ss", $config['dbname'], $config['categories'];
+	mysqli_stmt_prepare($querycategories_prepare, "SELECT `HeaderE` FROM `Categories`");
+	mysqli_stmt_bind_param($querycategories_prepare, "ss", $config['dbname'], $config['categories']);
 	mysqli_stmt_execute($querycategories_prepare);
 	mysqli_stmt_bind_result($querycategories_prepare,$col);
 		
