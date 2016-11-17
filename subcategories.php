23e111cbc9219e4b5af8abb04818fc04fc4211df
jlin044 <jlin044@uottawa.ca>
2016-11-17 13:59:50 -0500

    Update subcategories.php

--- subcategories.php
+++ subcategories.php
@@ -1,12 +1,10 @@
 <?php
-
-if(!empty($_POST["CategoryID"])) {
-	$query ="SELECT * FROM `SubCategories` WHERE CategoryID = '" . $_POST["CategoryID"] . "'";
+$dbhandle = mysqli_connect($config['hostname'], $config['username'], $config['password'],$config['dbname']) or die("Unable to connect to Server");
+$choice = mysql_real_escape_string($_GET['choice']);
+$query = "SELECT * FROM `SubCategories` WHERE CategoryID='$choice'";
+	
 	$results = $dbhandle->query($query);
 	mysqli_stmt_bind_result($results,$col1,$col2,$col3,$col4,$col5,$col6,$col7,$col8,$col9,$col10);
-?>
-	<option value="">Select a Category</option>
-<?php
 	while(mysqli_stmt_fetch($results)){
 		  echo "<option value=$col1>$col3 ($col2)</option>";
 	}
