0bc97b2454cb7182b9fc39a39f5a1810ce694910
Sugan21 <sugan.vivek@gmail.com>
2017-01-23 10:24:39 -0500

    Update ajaxData.php

--- ajaxData.php
+++ ajaxData.php
@@ -47,7 +47,7 @@ if(isset($_POST["subcategory_id"]) && !empty($_POST["subcategory_id"])){
 	 ?>
 
 <tr>
-  <td><p><?php CompanyName($row['CompanyID']) ?></p></td>
+  <td><p></p></td>
   <td><p><?php echo $row['NameE']; ?></p></td>
   <td><p><?php echo $row['ApprovalDate']; ?></p></td>
 </tr>
