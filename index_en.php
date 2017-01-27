594601d6ad0da9a990ef9d4cb1c39233f6c53420
Sugan21 <sugan.vivek@gmail.com>
2017-01-27 13:09:57 -0500

    Update index_en.php

--- index_en.php
+++ index_en.php
@@ -84,7 +84,7 @@ $db->set_charset("utf8");
     exit();
 }
 	
-	else { printf("<br />connected OO db4<br />");}
+	else { printf("<br />connected OO db5<br />");}
 	
 if ($result = $db->query("SELECT * FROM Categories")) {
     printf("Select returned %d rows.\n", $result->num_rows);
