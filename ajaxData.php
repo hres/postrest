e431efc75552fec6d5717a9d510aa32cd8cce062
Sugan21 <sugan.vivek@gmail.com>
2017-06-08 15:55:38 -0400

    Update ajaxData.php

--- ajaxData.php
+++ ajaxData.php
@@ -1868,7 +1868,7 @@ function paging($subID, $total, $pageid, $TotalCount)
   <br />
 </div>
 <?php
-    echo "<br /><br /><br />";
+    echo "<br />";
     if ($pageid > 1) {
         $id = $pageid - 1;
         echo "<div class='divClass'><a href='#' onclick='pages($subID, $id)'>Previous</a></div>";
