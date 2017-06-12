c04eb31aad4a3647d27375441b4bdab3d2be4250
Bimal Bhagrath <bimo.17@outlook.com>
2017-06-12 15:17:26 -0400

    general form template

---
+++ request.js
@@ -0,0 +1,6 @@
+// js file for producing http requests from submitted form in drug-query.html
+
+function prepare() {
+	var din = document.getElementById("din").value;
+	document.getElementById("test").innerHTML = "succeed";
+}
