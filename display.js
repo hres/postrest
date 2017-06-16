1897cfed73dea1c3a28997935a76b3d73759255f
Bimal Bhagrath <bimo.17@outlook.com>
2017-06-16 10:01:49 -0400

    Add files via upload

--- display.js
+++ display.js
@@ -12,14 +12,57 @@ var base = "https://rest-dev.hres.ca/rest-dev/drug_product?select=*,active_ingre
 
 $(document).ready(function() {
 	
-	console.log(true);
-	
 	query = base + "&drug_identification_number=eq." + drug_id;
 	
 	$.get(query, function(data) {
-		//console.log(data);
+		var obj = data[0];
+		//console.log(obj);
 		
+		if (data[0].class == "Veterinary") {
+			document.getElementById("speciesDiv").style.display = "block";
+		}
 		
+		document.getElementById("status").innerHTML = "---";
+		document.getElementById("statusDate").innerHTML = "---";
+		document.getElementById("market").innerHTML = "---";
+		document.getElementById("product").innerHTML = obj.brand_name;
+		document.getElementById("din").innerHTML = obj.drug_identification_number;
+		
+		var company = document.getElementById("company");
+		var c = obj.companies[0];
+		company.innerHTML = "<b>" + c.company_name + "</b>";
+		
+		if (c.suite_number != "") {
+			company.innerHTML += "<br>" + c.suite_number;
+		}
+		
+		company.innerHTML += "<br>" + c.street_name + "<br>" + c.city_name + ", " + c.province + "<br>" + c.country + " " + c.postal_code;
+		
+		document.getElementById("drugClass").innerHTML = obj.class;
+		//document.getElementById("species").innerHTML = obj.vet_species.species;
+		document.getElementById("dosage").innerHTML = "---";
+		document.getElementById("route").innerHTML = obj.route[0].route_of_administration;
+		document.getElementById("active").innerHTML = obj.number_of_ais;
+		document.getElementById("schedule").innerHTML = "---";
+		document.getElementById("ahfs").innerHTML = obj.therapeutic_class[0].tc_ahfs_number + " " + obj.therapeutic_class[0].tc_ahfs;
+		document.getElementById("atc").innerHTML = obj.therapeutic_class[0].tc_atc_number + " " + obj.therapeutic_class[0].tc_atc;
+		document.getElementById("aig").innerHTML = obj.ai_group_no;
+		
+		var table = document.getElementById("activeIngredientsTable");
+		
+		for (var i = 0; i < obj.active_ingredient.length; i++) {
+			var ingredientRow = table.insertRow(i + 1);
+			
+			var activeIngredientCell = ingredientRow.insertCell(0);
+			var strengthCell = ingredientRow.insertCell(1);
+			
+			activeIngredientCell.innerHTML = obj.active_ingredient[i].ingredient;
+			strengthCell.innerHTML = obj.active_ingredient[i].strength + " " + obj.active_ingredient[i].strength_unit;
+		}
+		
+		table.deleteRow(-1);
+		
+		var ingredientRow
 	});
 });
 
